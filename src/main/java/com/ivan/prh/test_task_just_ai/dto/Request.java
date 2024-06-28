06dab25a46bcb36c2fdef6a3e80c39508c829f11
ivan Prikhodko <ivanprh2004@gmail.com>
2024-06-28 15:20:46 +0300

    commit

--- src/main/java/com/ivan/prh/test_task_just_ai/dto/Request.java
+++ src/main/java/com/ivan/prh/test_task_just_ai/dto/Request.java
@@ -1,11 +1,13 @@
 package com.ivan.prh.test_task_just_ai.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.databind.JsonNode;
 
 public record Request(
     String type,
     String eventId,
-    double v,
+    @JsonAlias("v")
+    double version,
     JsonNode object,
     long groupId
 ){
